15b1a29c55b143be2a9f14737217a8e97eedb4a0
Jayanta Dewdhe <jayantadewdhe10@gmail.com>
2026-05-06 14:29:48 +0530

    prepared for the deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dns from "dns";
 import cors from "cors"
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./src/routes/notesRoutes.js";
 import connectDB from "./src/config/db.js";
@@ -15,15 +16,21 @@ dotenv.config();
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express();
+const __dirname = path.resolve()
 
 // Connect DB
 connectDB();
 
 // Middlewares
+
+if(process.env.NODE_ENV !== "production"){
 app.use(cors({
   origin: "http://localhost:5173"
 })
 );
+
+}
+
 
 app.use(express.json());
 app.use(rateLimiter);
@@ -36,6 +43,16 @@ app.use((req, res, next) => {
 
 // Routes
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+
+  app.use(express.static(frontendPath));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
 // Start server
 app.listen(5001, () => {
